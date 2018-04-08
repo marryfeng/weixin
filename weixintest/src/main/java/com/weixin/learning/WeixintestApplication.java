5885cd06fb2ce057b393c996682343e07e5172b4
imooc <1473357859@qq.com>
2018-04-08 20:25:07 +0800

    Initial commit

---
+++ weixintest/src/main/java/com/weixin/learning/WeixintestApplication.java
@@ -0,0 +1,12 @@
+package com.weixin.learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeixintestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeixintestApplication.class, args);
+	}
+}
